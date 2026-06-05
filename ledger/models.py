275777bedae99dedca9cc159de6a1ec18adeae2d
PolarBear <phoebechua124@gmail.com>
2026-06-05 14:16:25 +0800

    Fixed images styling and models

--- ledger/models.py
+++ ledger/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
-from django.core.validators import MinLengthValidator
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
-    short_bio = models.TextField(validators=[MinLengthValidator(256)])
+    short_bio = models.TextField()
 
     def __str__(self):
         return '{}'.format(self.name)
