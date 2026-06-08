02b14e5522b2294706dffc4015c4f63ac7f9104c
PolarBear <phoebechua124@gmail.com>
2026-06-08 13:23:44 +0800

    Changes

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 set -o errexit
 
 pip install -r requirements.txt
+mkdir -p static
+rm -rf staticfiles
 python manage.py tailwind install
 python manage.py tailwind build
 python manage.py collectstatic --noinput
