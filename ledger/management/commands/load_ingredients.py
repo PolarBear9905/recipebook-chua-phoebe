9e245ac298947edd695fff841f4a64bac81e065e
PolarBear <phoebechua124@gmail.com>
2026-06-08 16:35:50 +0800

    Edited terminal for command (load_ingredients)

--- ledger/management/commands/load_ingredients.py
+++ ledger/management/commands/load_ingredients.py
@@ -35,8 +35,11 @@ class Command(BaseCommand):
 
         ]
         for name in ingredients:
-           Ingredient.objects.get_or_create(name=name)
-           self.stdout.write(f'Added: {name}')
+            ingredient, created = Ingredient.objects.get_or_create(name=name)
+            if created:
+                self.stdout.write(f'Added: {name}')
+            else:
+                self.stdout.write(f'Already exists: {name}')
 
         self.stdout.write(
             self.style.SUCCESS('DONE!')
